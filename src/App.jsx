ff46d67266c5d4f09f33dde83453cf51b88a6a84
LevDer2 <123574272+LevDer2@users.noreply.github.com>
2026-04-17 21:01:15 +0300

    api

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,34 @@
 import { Component } from "react";
 import Searchbar from "./component/Searchbar/Searchbar";
+import { fetchImages } from "./api";
 
 class App extends Component {
-  state={
-    query:""
+  state = {
+    query: "",
+    page: 1,
+    images: [],
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.query !== this.state.query) {
+      return;
+    }
+
+    const {query, page} = this.state
+    fetchImages(query, page).then((res) => {
+      this.setState({images: res.hits})
+    })
   }
-  handleSearch=(query)=>{
-  this.setState({
-    query:query, 
-  });
-  
-  }
-  render(){
-    console.log(this.state.query);
-    
-    return(
-      <Searchbar onSubmit={this.handleSearch}/> 
-    )
+
+  handleSearch = (query) => {
+    this.setState({
+      query: query,
+      page: 1,
+      images: [],
+    });
+  };
+  render() {
+    return <Searchbar onSubmit={this.handleSearch} />;
   }
 }
 
--- src/component/Searchbar/Searchbar.jsx
+++ src/component/Searchbar/Searchbar.jsx
@@ -1,48 +1,39 @@
 import { Component } from "react";
 
 class Searchbar extends Component {
-    state={
-    query:""
+  state = {
+    query: "",
+  };
+
+  handleChange = (e) => {
+    this.setState({ query: e.target.value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    if (this.state.query === "") {
+      return;
     }
+    this.props.onSubmit(this.state.query);
+  };
 
-
-    handleChange=(e)=>{
-    this.setState({query:e.target.value})
-    }
-
-
-    handleSubmit=(e)=>{
-    e.preventDefault()
-    if(this.state.query===""){
-        return
-    }
-    this.props.onSubmit(this.state.query)
-    }
-
-
-    render(){
-        return(
-            <header >
-  <form onSubmit={this.handleSubmit}>
-    <button type="submit">
-      <span>Search</span>
-    </button>
-    <input
-    value={this.state.query}
-    onChange={this.handleChange}
-      type="text"
-      placeholder="Search images and photos"
-    />
-  </form>
-</header>
-        )
-    }
+  render() {
+    return (
+      <header>
+        <form onSubmit={this.handleSubmit}>
+          <button type="submit">
+            <span>Search</span>
+          </button>
+          <input
+            value={this.state.query}
+            onChange={this.handleChange}
+            type="text"
+            placeholder="Search images and photos"
+          />
+        </form>
+      </header>
+    );
+  }
 }
-
-
-
-
-
-
 
 export default Searchbar;
